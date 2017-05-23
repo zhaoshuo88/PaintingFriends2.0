e204775dd2143e8985421e80a8b1e99c4f0b2187
songhongxue <575899302@qq.com>
2017-05-23 10:30:30 +0800

    2017-5-23

---
+++ app/src/main/java/com/example/administrator/paintingfriends20/fragment/RequestAdapter.java
@@ -0,0 +1,65 @@
+package com.example.administrator.paintingfriends20.fragment;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+
+import com.example.administrator.paintingfriends20.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Loktar on 2017/5/23.
+ */
+public class RequestAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Request> lRequest = new ArrayList<>();
+
+    public RequestAdapter(Context context, List<Request> lRequest) {
+        this.context = context;
+        this.lRequest = lRequest;
+    }
+
+    @Override
+    public int getCount() {
+        return lRequest.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return lRequest.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return lRequest.get(i).getId();
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View v;
+        if(null == view){
+            v = LayoutInflater.from(context).inflate(R.layout.layout_request_item,null);
+
+        }else {
+            v = view;
+        }
+        ImageButton headPhoto = (ImageButton) v.findViewById(R.id.IbReqitemHeadphoto);
+        headPhoto.setImageResource(lRequest.get(i).getHeadPhoto());
+        TextView TvName = (TextView) v.findViewById(R.id.TvReqitemName);
+        TvName.setText(lRequest.get(i).getName());
+        TextView TvRequest = (TextView) v.findViewById(R.id.TvReqitemRequest);
+        TvRequest.setText(lRequest.get(i).getRequest());
+        TextView TvTime = (TextView) v.findViewById(R.id.TvReqitemTime);
+        TvTime.setText(lRequest.get(i).getTime());
+
+        return v;
+    }
+}
--- app/src/main/java/com/example/administrator/paintingfriends20/fragment/RequestFragment.java
+++ app/src/main/java/com/example/administrator/paintingfriends20/fragment/RequestFragment.java
@@ -6,8 +6,14 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.administrator.paintingfriends20.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 15530 on 2017/5/21.
@@ -16,12 +22,43 @@ import com.example.administrator.paintingfriends20.R;
 public class RequestFragment extends Fragment {
 
     private View view;
+    private ImageButton mHeadphoto;
+    private TextView mName;
+    private TextView mRequest;
+    private TextView mTime;
+    private List<Request> lRequest = new ArrayList<>();
+    private RequestAdapter requestAdapter;
+    private ListView mRequestList;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
-        view = inflater.inflate(R.layout.activity_request,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_request, container, false);
+
+        findView();
+        getData();
+
+        requestAdapter = new RequestAdapter(getActivity(),lRequest);
+
         return view;
+    }
+    private void findView() {
+        mRequestList = (ListView)view.findViewById(R.id.LvReqAdpview);
+        mHeadphoto = (ImageButton)view.findViewById(R.id.IbReqitemHeadphoto);
+        mName = (TextView)view.findViewById(R.id.TvReqitemName);
+        mRequest = (TextView)view.findViewById(R.id.TvReqitemRequest);
+        mTime = (TextView)view.findViewById(R.id.TvReqitemTime);
+    }
+    private void getData(){
+        for (int i = 0;i < 10; i++){
+            int rid = i;
+            String rdate = "2017-5-23";
+            String redetail = "这个是详细需求需求";
+            int ruid = 345678;
+            String name = "画友第"+ (i+1) +"号用户";
+            lRequest.add(new Request((long)rid,R.drawable.touxiang3,name,redetail,rdate));
+            System.out.println(rid + rdate + redetail + ruid);
+            mRequestList.setAdapter(requestAdapter);
+        }
     }
 }
