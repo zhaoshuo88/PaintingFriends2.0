a7029d7ee1e69d96ab0e7334c378ce8de29d58c1
songhongxue <575899302@qq.com>
2017-06-01 16:11:27 +0800

    添加返回按钮，但是返回到fragment不会写，只写了返回Activity，待我研究后补全

--- app/src/main/java/com/example/administrator/paintingfriends20/MainActivity.java
+++ app/src/main/java/com/example/administrator/paintingfriends20/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         //1.获取界面的控件
         getViews();
 
--- app/src/main/java/com/example/administrator/paintingfriends20/ui/MyRequestsActivity.java
+++ app/src/main/java/com/example/administrator/paintingfriends20/ui/MyRequestsActivity.java
@@ -1,19 +1,24 @@
 package com.example.administrator.paintingfriends20.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.administrator.paintingfriends20.MainActivity;
 import com.example.administrator.paintingfriends20.R;
 import com.example.administrator.paintingfriends20.adapter.HomeLikeAdapter;
 import com.example.administrator.paintingfriends20.adapter.MyRequestAdapter;
 import com.example.administrator.paintingfriends20.domain.HomeLike;
 import com.example.administrator.paintingfriends20.domain.MyRequestDomain;
+import com.example.administrator.paintingfriends20.fragment.MineFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +27,7 @@ public class MyRequestsActivity extends Activity {
 
     private ListView mLvReqAdpview;
     private List<MyRequestDomain> requestLists = new ArrayList<>();
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +37,17 @@ public class MyRequestsActivity extends Activity {
         getrequestListsDate();
         MyRequestAdapter adapter = new MyRequestAdapter(getApplicationContext(), requestLists);
         mLvReqAdpview.setAdapter(adapter);
+
+        btn = (Button)findViewById(R.id.btn_request);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MyRequestsActivity.this, MainActivity.class);
+                //intent.putExtra("page",4);
+                startActivity(intent);
+            }
+        });
     }
 
     private void getrequestListsDate() {
